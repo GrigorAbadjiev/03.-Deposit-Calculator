e0452474b7dcf133cb9522d9547e09a7fbb51396
nanji <nanji@DESKTOP-4A8MIDK>
2022-10-30 12:05:39 +0200

    Add project files.

---
+++ 03. Deposit Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // input
+           double depositSum = double.Parse(Console.ReadLine());
+            int depositInMonths = int.Parse(Console.ReadLine());
+            double yearInterestPercentage = double.Parse(Console.ReadLine());
+            
+            double yearleAccumulatedInterest = depositSum * yearInterestPercentage / 100;
+            // calculate
+            double totalSum = depositSum + depositInMonths * ((depositSum * yearInterestPercentage / 100 / 12));
+
+
+            // output
+            Console.WriteLine(totalSum);
+
+        }
+    }
+}
